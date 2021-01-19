680fd5dab61d75ce406204cf425f81b4e0ba68d6
Filippo <ondaradiofm@gmail.com>
2021-01-19 19:39:55 +0100

    Multiple patients

--- test/main.py
+++ test/main.py
@@ -1,87 +1,108 @@
 import gensim.downloader as api
 import math
 import matplotlib.pyplot as plt
+import random
 
-NOW_PROCESSING = "Now processing ({0}, {1})"
+NOW_PROCESSING = "Now processing ({0}, {1}) for patient {2}"
 FOUND_MATCH = "Found a match in MEN database for ({0}, {1})!"
 MEN_DISTANCE = "Distance from word2vec and MEN: {0}"
-DONE = "Final score: {0}"
+DONE = "Final score for patient {1}: {0}"
 
 FOUND_CORRELATION = "Target word ({0}) has a difficulty rating!"
-CORRELATION = "The Spearman correlation for this dataset is: {0}"
+CORRELATION = "The Spearman correlation for patient {1} is: {0}"
 
 def main():
     # Load our models
-    wv = api.load("word2vec-google-news-300")
+    #wv = api.load("word2vec-google-news-300")
     men = tupleize("data/men.csv")
 
     # First, extract our target - response pairs
     input = tupleize("input.csv")
 
+    temp = {}
+    for t in input:
+        key = int(t[0])
+        if key in temp:
+            temp[key].append(t[1:])
+        else:
+            temp[key] = [t[1:]]
+
     # Of course we are also going to need some "difficulty" metric
     difficulty = dict(tupleize("data/difficulty.csv"))
-    # This will contain tuples with the target word, its difficulty
-    # and the final score (relative to the response word)
-    correlation = []
     # Then, get similarity estimates from our models
     with open("output.csv", "w") as output:
-        for target, response in input:
-            print(NOW_PROCESSING.format(target, response));
-            wvScore = wv.similarity(target, response)
+        for patient in temp:
+            # This will contain tuples with the target word, its difficulty
+            # and the final score (relative to the response word)
+            correlation = []
 
-            # If available, extract similarity estimate
-            # from MAN as well...
-            match = []
-            for i in men:
-                if target in i and response in i:
-                    match.append(i)
+            pairs = temp[patient]
 
-            if len(match) == 1:
-                # If found normalize it...
-                print(FOUND_MATCH.format(target, response))
-                menScore = float(match[0][2]) / 50.0
-                # ...find the vector distance from the two models...
-                distance = abs(menScore - wvScore)
-                print(MEN_DISTANCE.format(distance))
-                # ...and get a final value as the mean of the two scores
-                meanScore = mean([wvScore, menScore])
-            else:
-                # Too bad, can't do anything about it...
-                meanScore = wvScore
+# ==============================================================================
 
-            print(DONE.format(meanScore));
-            # Write our results in output.csv
-            output.write(", ".join([target, response, str(meanScore)]))
+            for target, response in pairs:
+                print(NOW_PROCESSING.format(target, response, patient));
+                #wvScore = wv.similarity(target, response)
+                wvScore = random.random()
+
+                # If available, extract similarity estimate
+                # from MAN as well...
+                match = []
+                for i in men:
+                    if target in i and response in i:
+                        match.append(i)
+
+                if len(match) == 1:
+                    # If found normalize it...
+                    print(FOUND_MATCH.format(target, response))
+                    menScore = float(match[0][2]) / 50.0
+                    # ...find the vector distance from the two models...
+                    distance = abs(menScore - wvScore)
+                    print(MEN_DISTANCE.format(distance))
+                    # ...and get a final value as the mean of the two scores
+                    meanScore = mean([wvScore, menScore])
+                else:
+                    # Too bad, can't do anything about it...
+                    meanScore = wvScore
+
+                print(DONE.format(meanScore, patient));
+                # Write our results in output.csv
+                output.write(", ".join(
+                    [str(patient), target, response, str(meanScore)]))
+
+                output.write("\n")
+
+                # Now working on our correlation coefficient
+                #
+                # First, we need to keep track of which target words
+                # have a difficulty score associated to them. We'll only
+                # be able to use those to calculate our correlation coefficient.
+                if target in difficulty:
+                    print(FOUND_CORRELATION.format(target), end="\n\n")
+                    # Keep track of the word, its difficulty score and the mean
+                    # similarity with the response word.
+                    # Of course there can be more than one entry for a
+                    # given target word.
+                    tuple = (target, float(difficulty[target]), meanScore)
+                    correlation.append(tuple)
+
+# ==============================================================================
             output.write("\n")
 
-            # Now working on our correlation coefficient
-            #
-            # First, we need to keep track of which target words
-            # have a difficulty score associated to them. We'll only
-            # be able to use those to calculate our correlation coefficient.
-            if target in difficulty:
-                print(FOUND_CORRELATION.format(target), end="\n\n")
-                # Keep track of the word, its difficulty score and the mean
-                # similarity with the response word.
-                # Of course there can be more than one entry for a
-                # given target word.
-                tuple = (target, float(difficulty[target]), meanScore)
-                correlation.append(tuple)
+            # Now that we know which target words can be used
+            # for the evaluation of our correlation coefficient
+            # we need to construct the variables' rankings
+            xrank = rank([i[1] for i in correlation], False)
+            yrank = rank([i[2] for i in correlation], True)
 
-    # Now that we know which target words can be used
-    # for the evaluation of our correlation coefficient
-    # we need to construct the variables' rankings
-    xrank = rank([i[1] for i in correlation], False)
-    yrank = rank([i[2] for i in correlation], True)
+            covariance = cov(list(zip(xrank, yrank)))
 
-    covariance = cov(list(zip(xrank, yrank)))
+            spearman = covariance / math.sqrt(var(xrank) * var(yrank))
+            print(CORRELATION.format(spearman, patient))
 
-    spearman = covariance / math.sqrt(var(xrank) * var(yrank))
-    print(CORRELATION.format(spearman))
-
-    # Might be interesting...
-    plt.scatter(xrank, yrank)
-    plt.show()
+            output.write(", ".join(
+                [str(patient), str(spearman)]))
+            output.write("\n\n")
 
 # Quick&dirty implementation of the fractional sorting
 # algo, Spearman works best with this...
